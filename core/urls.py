03eee3c3e1cb7792b98af2a4963aa898f395268b
kennystewart <kennystwork@gmail.com>
2020-08-02 16:56:27 +0800

    product view

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import (
     products,
     checkout,
-    home
+    HomeView
 )
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name='checkout'),
     path('products/', products, name='products')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView 
+from django.views.generic import ListView, DetailView
 from .models import Item
 
 
@@ -14,8 +14,11 @@ def checkout(request):
     return render(request, "checkout.html")
 
 
-def home(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home.html",context)
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"

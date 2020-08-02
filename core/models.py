a066a7420b227884f08aa722e7c52ad383194e23
kennystewart <kennystwork@gmail.com>
2020-08-03 00:39:32 +0800

    home page item image add click link & models add description

--- core/models.py
+++ core/models.py
@@ -23,6 +23,7 @@ class Item(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title

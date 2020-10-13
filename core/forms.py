45fe1b19804d3799ea49f3eea443f5d5e6fb32ab
kennystewart <kennystwork@gmail.com>
2020-10-13 11:16:56 +0800

    payment option 1:48:45

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from django_countries.fields import CountryField
 
 
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+
 class CheckoutForm(forms.Form):
     street_address = forms.CharField()
     apartment_address = forms.CharField(required=False)
@@ -9,5 +14,4 @@ class CheckoutForm(forms.Form):
     zip = forms.CharField()
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
-    
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect,choices=PAYMENT_CHOICES)

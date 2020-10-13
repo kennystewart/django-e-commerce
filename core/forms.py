7712cc3313bebf94324fb2171c2d3c11184919b2
kennystewart <kennystwork@gmail.com>
2020-10-13 11:34:55 +0800

    checkout forms modified 1:52:13

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
 
 
 PAYMENT_CHOICES = (
@@ -7,11 +8,26 @@ PAYMENT_CHOICES = (
     ('P', 'PayPal')
 )
 
+
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
-    zip = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.ChoiceField(widget=forms.RadioSelect,choices=PAYMENT_CHOICES)
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '1234 Main St',
+        'id': 'address'
+    }))
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or suite',
+        'id': 'address-2'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100',
+
+        }))
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'id': 'zip'
+    }))
+    same_shipping_address = forms.BooleanField(required=False)
+    save_info = forms.BooleanField(required=False)
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
